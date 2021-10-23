f0f4f025a0ad8fae8f039bb9f637c0633040cd90
Charipi <cburometto3@gmail.com>
2021-10-23 09:42:56 -0400

    Added code to keep wheel angles the same when turning left and right

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -318,6 +318,13 @@ public class Drive {
         swervePower = Math.sqrt((swerveX * swerveX) + (swerveY * swerveY));
         swerveAngle = Math.toDegrees(Math.atan2(swerveX, swerveY));
 
+        //If we are rotating CCW, and we are not crab driving, then the robot will flip the wheel angles and powers
+        //This keeps the wheels in the same position when turning both ways, making small rotations easier
+        if ((rotatePower < 0) && (crabX == 0 && crabY == 0)) {
+            swervePower *= -1;
+            swerveAngle += 180;
+        }
+
         PowerAndAngle swerveNums = new PowerAndAngle(swervePower, swerveAngle);
 
         return swerveNums;
